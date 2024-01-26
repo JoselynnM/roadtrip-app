0fbbfa1fdc11c6d8b44a2fe962c26358ba645bc6
Joselyn <joselyn.moreta@chasacademy.se>
2024-01-26 23:52:52 +0100

    edit

--- frontend/src/pages/Help.tsx
+++ frontend/src/pages/Help.tsx
@@ -14,7 +14,7 @@ import {
   Button,
 } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
-import image from "../assets/bg12.png";
+import image from "../assets/bg13.jpg";
 
 
 const Help: React.FC = () => {
